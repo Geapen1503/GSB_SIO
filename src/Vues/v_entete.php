85023cdfed38d0b6edc5f6435704234bdd6aac1d
Geapen <theogilat@gmail.com>
2024-09-20 16:19:37 +0200

    Modifying header for the comptable part

--- src/Vues/v_entete.php
+++ src/Vues/v_entete.php
@@ -33,7 +33,7 @@
         <div class="container">
             <?php
             $uc = filter_input(INPUT_GET, 'uc', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            if ($estConnecte) {
+            if ($estConnecte && $_SESSION['typeUtilisateur'] == 'visiteur') {
                 ?>
             <div class="header">
                 <div class="row vertical-align">
@@ -65,6 +65,49 @@
                                 </a>
                             </li>
                             <li 
+                            <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
+                                <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
+                                    <span class="glyphicon glyphicon-log-out"></span>
+                                    Déconnexion
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <?php
+            } elseif ($estConnecte && $_SESSION['typeUtilisateur'] == 'comptable') {
+            ?>
+                <div class="header">
+                <div class="row vertical-align">
+                    <div class="col-md-4">
+                        <h1>
+                            <img src="./images/logo.jpg" class="img-responsive"
+                                 alt="Laboratoire Galaxy-Swiss Bourdin"
+                                 title="Laboratoire Galaxy-Swiss Bourdin">
+                        </h1>
+                    </div>
+                    <div class="col-md-8">
+                        <ul class="nav nav-pills pull-right" role="tablist">
+                            <li <?php if (!$uc || $uc == 'accueil') { ?>class="active" <?php } ?>>
+                                <a href="index.php">
+                                    <span class="glyphicon glyphicon-home"></span>
+                                    Accueil
+                                </a>
+                            </li>
+                            <li <?php if ($uc == 'gererFrais') { ?>class="active"<?php } ?>>
+                                <a href="index.php?uc=gererFrais&action=saisirFrais">
+                                    <span class="glyphicon glyphicon-euro"></span>
+                                    Valider les fiches de frais
+                                </a>
+                            </li>
+                            <li <?php if ($uc == 'etatFrais') { ?>class="active"<?php } ?>>
+                                <a href="index.php?uc=etatFrais&action=selectionnerMois">
+                                    <span class="glyphicon glyphicon-euro"></span>
+                                    Suivre le paiement des fiches de frais
+                                </a>
+                            </li>
+                            <li
                             <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
                                 <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
                                     <span class="glyphicon glyphicon-log-out"></span>
