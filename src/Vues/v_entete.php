1c1d75e08c6e88f1778686470df1453b6ef5ba07
JeffAst <geoffrey.astesiano@gmail.com>
2024-10-03 08:41:00 +0200

    Couleur orange pour l'entête

--- src/Vues/v_entete.php
+++ src/Vues/v_entete.php
@@ -90,26 +90,26 @@
                     <div class="col-md-8">
                         <ul class="nav nav-pills2 pull-right" role="tablist">
                             <li <?php if (!$uc || $uc == 'accueil') { ?>class="active" <?php } ?>>
-                                <a href="index.php">
+                                <a class="orgcomptable" href="index.php" >
                                     <span class="glyphicon glyphicon-home"></span>
                                     Accueil
                                 </a>
                             </li>
                             <li <?php if ($uc == 'gererFrais') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=gererFrais&action=saisirFrais">
+                                <a class="orgcomptable" href="index.php?uc=gererFrais&action=saisirFrais">
                                     <span class="glyphicon glyphicon-euro"></span>
                                     Valider les fiches de frais
                                 </a>
                             </li>
                             <li <?php if ($uc == 'etatFrais') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=etatFrais&action=selectionnerMois">
+                                <a class="orgcomptable" href="index.php?uc=etatFrais&action=selectionnerMois">
                                     <span class="glyphicon glyphicon-euro"></span>
                                     Suivre le paiement des fiches de frais
                                 </a>
                             </li>
                             <li
                             <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
+                                <a class="orgcomptable" href="index.php?uc=deconnexion&action=demandeDeconnexion">
                                     <span class="glyphicon glyphicon-log-out"></span>
                                     Déconnexion
                                 </a>
